183c88b29d7808c1b9b07ba792051628791d6517
harleenkaur2003 <160705023+harleenkaur2003@users.noreply.github.com>
2025-03-13 08:39:24 +0530

    merge conflict 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,5 @@
 import React from 'react'
-<<<<<<< HEAD
 import Home from './PagesTemp/Home.js'
-=======
-import Home from './PagesTemp/Home'
->>>>>>> 53410776a83961a8b72691e1691c9c4145b3b04d
 import BsState from './contextTemp/BsState'
 
 const App = () => {
