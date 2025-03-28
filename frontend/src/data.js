5294188e508ea04100c69f00e36fce0af14e133f
Harleen Kaur <160705023+harleenkaur2003@users.noreply.github.com>
2025-03-28 15:29:21 +0530

    Update data.js

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,7 +1,4 @@
-movieList=['Interstellar', 'Tenet', 'Shutter Island','Matrix','Fast X']
-slots=['10 AM','1 PM' , '4 PM','7 PM']
-seats=['A1','A2','A3','B1','B2','B3','C1','C2','C3']
+export const movieList=['Interstellar', 'Tenet', 'Shutter Island','Matrix','Fast X']
+export const slots=['10 AM','1 PM' , '4 PM','7 PM']
+export const seats=['A1','A2','A3','B1','B2','B3','C1','C2','C3']
 
-exports.movieList=movieList;
-exports.slots=slots;
-exports.seats= seats;
